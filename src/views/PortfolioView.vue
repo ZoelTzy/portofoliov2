6cd8cf56bd28f7f354113a2b7664a93adbf3ffe2
ZoelTzy <103124907+ZoelTzy@users.noreply.github.com>
2025-04-04 22:52:44 +0700

    Update PortfolioView.vue

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -10,7 +10,7 @@ export default {
           imageUrl: 'portfolio_v2',
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
           tech: 'VueJS 3, Tailwind',
-          github: 'https://github.com/ZoelTzy/PortofolioV3',
+          github: 'https://github.com/ZoelTzy/portofoliov2',
           demo: 'https://ifern.my.id'
         },
       ]
